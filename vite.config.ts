8a5b7c486db1b09b569b0c3feaf72f6b1233d81e
LILUTECNO <lilutecno@gmail.com>
2025-07-04 13:23:01 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lilutecnopagina/', // <-- ¿Está esta línea exactamente así?
-})
+  base: base: '/'
